e3e168149d90f1d60afc5f658543797a3c84cc07
Michel Hoffmeister <michel.hoffmeister@stud.hs-ruhrwest.de>
2024-07-30 11:55:33 +0200

    hotfix

--- django_test/settings.py
+++ django_test/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,21 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-e&+24_*76l37@n+1-(klxz_fcg&sfow=7=@6yy^a5lgi2h*blh"
-if os.environ.get("SECRET_KEY") != None:
+if os.environ.get("SECRET_KEY") is not None:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-print(SECRET_KEY)
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-if os.environ.get("DEBUG") != None:
+if os.environ.get("DEBUG") is not None:
     DEBUG = bool(os.environ.get("DEBUG"))
-print(DEBUG)
 
 ALLOWED_HOSTS = []
-if os.environ.get("ALLOWED_HOSTS") != None:
+if os.environ.get("ALLOWED_HOSTS") is not None:
     ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
-print(ALLOWED_HOSTS)
 
 # Application definition
 
@@ -127,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
